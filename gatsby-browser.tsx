798fc69ba3ec612838ec1e2958f35c8069e63261
Robbie <robbie@reformcollective.com>
2022-05-04 11:13:47 -0600

    reactify

--- gatsby-browser.tsx
+++
@@ -1,7 +0,0 @@
-import "styles/reset.css";
-import gsap from "gsap-trial";
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-// import type { GatsbyBrowser } from "gatsby"
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import IndexPage from "./components";
+
+function App() {
+  return <IndexPage />;
+}
+
+export default App;
