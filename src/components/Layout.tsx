faeed08781ed902ada0bc35da6f378dd3e71e683
RJWadley <robbiethewadley@gmail.com>
2022-05-04 09:38:39 -0700

    remove styled

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Scroll from "components/Scroll";
